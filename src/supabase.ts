2354246687d0f51e28109603fb51a4a6be209011
abbasabro <abroabbas44@gmail.com>
2026-03-31 21:31:27 +0500

    supase update

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,7 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://cbbhamqgqpzztdhquoet.supabase.co';
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY || 'sb_publishable_rIfy6wkt_jBkd3jzuo-KAg_QNksISaU';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Missing Supabase environment variables");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
